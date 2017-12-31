99b071c16d9f43614ae8f767c8be38a716c49cb5
jold01 <jack.old@outlook.com>
2017-12-31 02:48:52 -0500

    Successfully created Transition test

--- transition test/TranTest.java
+++ transition test/TranTest.java
@@ -5,6 +5,8 @@ import java.awt.event.*;
 public class TranTest extends JFrame{
    
    private JButton jbTran;
+	private JButton jbTran2;
+	private JTextField jtf2;
    
    public static void main(String[] args){
    
@@ -12,6 +14,8 @@ public class TranTest extends JFrame{
    }
    
    public TranTest(){
+
+		JFrame jfMain = new JFrame();
    
       JPanel jpTest = new JPanel();
    
@@ -19,17 +23,61 @@ public class TranTest extends JFrame{
       
       jpTest.add(jbTran);
       
-      add(jpTest);
+      jfMain.add(jpTest);
       
-      setTitle("TEST");
-      
-      pack();
-      setLocationRelativeTo(null);
-      setDefaultCloseOperation(EXIT_ON_CLOSE);
-      setVisible(true);
+      jfMain.setTitle("TEST");
+     
+		jbTran.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent ae){
+				
+				jfMain.dispose();
+				new Transition();
+			}
+
+		});
+
+     	jfMain.pack();
+      jfMain.setLocationRelativeTo(null);
+      jfMain.setDefaultCloseOperation(EXIT_ON_CLOSE);
+      jfMain.setVisible(true);
       
    
    
    }
+
+	class Transition{
+		
+		public Transition(){
+			JFrame newFrame = new JFrame();
+
+			JPanel jpMain = new JPanel(new GridLayout(0,1));
+
+			jbTran2 = new JButton("Test2");
+
+			jtf2 = new JTextField(10);
+
+			jpMain.add(jbTran2);
+			jpMain.add(jtf2);
+
+			newFrame.add(jpMain);
+
+			jbTran2.addActionListener(new ActionListener(){
+			
+				public void actionPerformed(ActionEvent ae){
+					
+					newFrame.dispose();
+					new TranTest();
+				}
+
+			});
+			
+			newFrame.pack();
+			newFrame.setLocationRelativeTo(null);
+			newFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			newFrame.setVisible(true);
+
+		}
+	}
 
 }
